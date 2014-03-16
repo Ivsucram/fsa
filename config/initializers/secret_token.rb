5000e1e209485e6ba832e7639f67c37b340b3b4b
Marcus Vinicius <ivsucram@gmail.com>
2014-03-16 16:41:00 +0800

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fsa::Application.config.secret_key_base = 'f1bf06c2a5fa001b5d60f8b591579d1f0d43ea222eefc4bd8dbd288e488c1fc7269a88817f44bb394b127f9f373fa67df0776b21e7ab843a146e982519cdc121'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fsa::Application.config.session_store :cookie_store, key: '_fsa_session'
